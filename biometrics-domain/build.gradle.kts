e4136ac791ba20831dfd52266a8f0438b852d6d1
Mykyta Sikriier <sikrinick@gmail.com>
2023-11-08 12:18:06 +0100

    [MAINTENANCE] Removed sources

--- biometrics-domain/build.gradle.kts
+++ biometrics-domain/build.gradle.kts
@@ -3,10 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.jvm")
 }
 
-tasks.jar {
-    from(sourceSets.main.get().allSource)
-}
-
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
