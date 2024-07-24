ae11e88bf550594b49bf901d501f94499cfd65af
e-snni <szymon.nitecki@gft.com>
2024-07-24 13:37:55 +0200

    [Maintenance] Add Maven publishing configuration to :biometric:android

--- biometric-android/build.gradle.kts
+++ biometric-android/build.gradle.kts
@@ -1,6 +1,10 @@
+import com.vanniktech.maven.publish.AndroidMultiVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("com.vanniktech.maven.publish")
 }
 
 android {
@@ -43,14 +47,51 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+mavenPublishing {
+    configure(
+        AndroidMultiVariantLibrary(
+            sourcesJar = true,
+            publishJavadocJar = true,
+        )
+    )
+    coordinates(project.property("libraryGroupId") as String, "biometric-android", project.property("libraryVersion") as String)
+
+    pom {
+        name.set(project.property("libraryNamePrefix") as String + " Android")
+        description.set(project.property("libraryDescription") as String)
+        inceptionYear.set(project.property("libraryInceptionYear") as String)
+        url.set("https://${project.property("libraryRepositoryUrl") as String}")
+        licenses {
+            license {
+                name.set(project.property("libraryLicenseName") as String)
+                url.set(project.property("libraryLicenseUrl") as String)
+                distribution.set(project.property("libraryLicenseDistribution") as String)
+            }
+        }
+        developers {
+            developer {
+                name.set(project.property("libraryDeveloperName") as String)
+            }
+        }
+        scm {
+            url.set("https://${project.property("libraryRepositoryUrl") as String}")
+            connection.set("scm:git:git://${project.property("libraryRepositoryUrl") as String}")
+            developerConnection.set("scm:git:ssh://git@${project.property("libraryRepositoryUrl") as String}.git")
+        }
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+        signAllPublications()
+    }
+}
+
+
 dependencies {
+    api(project(":biometric-api"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.biometric:biometric:1.1.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation(project(mapOf("path" to ":biometric-api")))
 
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
     testImplementation("io.mockk:mockk:1.13.9")
